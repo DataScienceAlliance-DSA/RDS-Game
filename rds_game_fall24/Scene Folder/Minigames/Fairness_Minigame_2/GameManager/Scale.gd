30d8fb7231775eddd993cd3c7584d674dc78be62
Diego Pereyra <23diegopereyra@gmail.com>
2025-02-10 11:47:11 -0800

    rotation for scale fixed

--- rds_game_fall24/Scene Folder/Minigames/Fairness_Minigame_2/GameManager/Scale.gd
+++ rds_game_fall24/Scene Folder/Minigames/Fairness_Minigame_2/GameManager/Scale.gd
@@ -4,12 +4,16 @@ extends CanvasLayer
 var current_tilt : float = 0.0  # Current tilt angle in degrees
 var beam : Sprite2D
 var left_pan: Sprite2D
+var left_pan_rotate: Control
 var right_pan: Sprite2D
+var right_pan_rotate: Control
 
 func _ready():
 	beam = $ScaleContainer/Beam  # Adjust the path to your Beam node
-	left_pan = beam.get_node("Left_Pan")
-	right_pan = beam.get_node("Right_Pan")
+	left_pan_rotate = get_node("LeftPanRotate")
+	left_pan = get_node("LeftPanRotate/Left_Pan")
+	right_pan_rotate = get_node("RightPanRotate")
+	right_pan = get_node("RightPanRotate/RightPan")
 	
 func _process(delta):
 	if Input.is_action_pressed("left"):
@@ -19,5 +23,9 @@ func _process(delta):
 		
 	else:
 		current_tilt = lerp(current_tilt, 0.0, 0.1)
-
+	
 	beam.rotation = deg_to_rad(current_tilt)
+	left_pan_rotate.rotation = deg_to_rad(current_tilt)
+	right_pan_rotate.rotation = deg_to_rad(current_tilt)
+	left_pan.rotation = deg_to_rad(-current_tilt)
+	right_pan.rotation = deg_to_rad(-current_tilt)
