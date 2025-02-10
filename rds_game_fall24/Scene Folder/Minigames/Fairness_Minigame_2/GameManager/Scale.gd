5016b47518a53fcc670bf1c55b8f50e280d7f4a4
Diego Pereyra <23diegopereyra@gmail.com>
2025-02-10 14:22:28 -0800

    scale component rotations lerp based on weight of objects placed

--- rds_game_fall24/Scene Folder/Minigames/Fairness_Minigame_2/GameManager/Scale.gd
+++ rds_game_fall24/Scene Folder/Minigames/Fairness_Minigame_2/GameManager/Scale.gd
@@ -8,6 +8,9 @@ var left_pan_rotate: Control
 var right_pan: Sprite2D
 var right_pan_rotate: Control
 
+@onready var left_pan_spots = get_tree().get_nodes_in_group("LeftPan")[0].get_children()
+@onready var right_pan_spots = get_tree().get_nodes_in_group("RightPan")[0].get_children()
+
 func _ready():
 	beam = $Control/Control/ScaleContainer/Beam  # Adjust the path to your Beam node
 	left_pan_rotate = get_node("Control/Control/LeftPanRotate")
@@ -18,12 +21,27 @@ func _ready():
 	# drop_spot_right = get_node("../DropSpotRight")
 
 func _process(delta):
-	if Input.is_action_pressed("left"):
-		current_tilt = clamp(current_tilt - 1, -tilt_angle, tilt_angle)
-	elif Input.is_action_pressed("right"):
-		current_tilt = clamp(current_tilt + 1, -tilt_angle, tilt_angle)
-	else:
-		current_tilt = lerp(current_tilt, 0.0, 0.1)
+	# if Input.is_action_pressed("left"):
+	# 	current_tilt = clamp(current_tilt - 1, -tilt_angle, tilt_angle)
+	# elif Input.is_action_pressed("right"):
+	# 	current_tilt = clamp(current_tilt + 1, -tilt_angle, tilt_angle)
+	# else:
+	# 	current_tilt = lerp(current_tilt, 0.0, 0.1)
+	
+	var weight_score : float = 0
+	
+	for spot in left_pan_spots:
+		for stuff in spot.get_children():
+			if (stuff.get_child_count() != 0):
+				weight_score -= stuff.name.to_float()
+	
+	for spot in right_pan_spots:
+		for stuff in spot.get_children():
+			if (stuff.get_child_count() != 0):
+				weight_score += stuff.name.to_float()
+	
+	var new_tilt = weight_score / 75. * tilt_angle
+	current_tilt = lerpf(current_tilt, new_tilt, delta * 3.)
 	
 	beam.rotation = deg_to_rad(current_tilt)
 	left_pan_rotate.rotation = deg_to_rad(current_tilt)
