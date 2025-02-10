d01fa106e30b3ca46bf3b755de60fc8d13b46bfb
Diego Pereyra <23diegopereyra@gmail.com>
2025-02-10 13:09:05 -0800

    drag & drop integration for scale

--- rds_game_fall24/Scene Folder/Minigames/Fairness_Minigame_2/GameManager/Scale.gd
+++ rds_game_fall24/Scene Folder/Minigames/Fairness_Minigame_2/GameManager/Scale.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends MarginContainer
 
 @export var tilt_angle : float = 25.0  # Maximum tilt angle in degrees
 var current_tilt : float = 0.0  # Current tilt angle in degrees
@@ -9,18 +9,19 @@ var right_pan: Sprite2D
 var right_pan_rotate: Control
 
 func _ready():
-	beam = $ScaleContainer/Beam  # Adjust the path to your Beam node
-	left_pan_rotate = get_node("LeftPanRotate")
-	left_pan = get_node("LeftPanRotate/Left_Pan")
-	right_pan_rotate = get_node("RightPanRotate")
-	right_pan = get_node("RightPanRotate/RightPan")
-	
+	beam = $Control/Control/ScaleContainer/Beam  # Adjust the path to your Beam node
+	left_pan_rotate = get_node("Control/Control/LeftPanRotate")
+	left_pan = get_node("Control/Control/LeftPanRotate/Left_Pan")
+	right_pan_rotate = get_node("Control/Control/RightPanRotate")
+	right_pan = get_node("Control/Control/RightPanRotate/RightPan")
+	# drop_spot_left = get_node("../DropSpotLeft")
+	# drop_spot_right = get_node("../DropSpotRight")
+
 func _process(delta):
 	if Input.is_action_pressed("left"):
 		current_tilt = clamp(current_tilt - 1, -tilt_angle, tilt_angle)
 	elif Input.is_action_pressed("right"):
 		current_tilt = clamp(current_tilt + 1, -tilt_angle, tilt_angle)
-		
 	else:
 		current_tilt = lerp(current_tilt, 0.0, 0.1)
 	
--- rds_game_fall24/Scene Folder/Minigames/Mixing_Game/DragAndDrop/draggable.gd
+++ rds_game_fall24/Scene Folder/Minigames/Mixing_Game/DragAndDrop/draggable.gd
@@ -5,10 +5,16 @@ var mouse_offset #center mouse on click
 var delay = 0.1
 var drop_spots
 var start_position = Vector2()
+var snap_position
+var current_spot
 
 func _ready():
 	start_position = self.position
 	drop_spots = get_tree().get_nodes_in_group('DropSpotGroup')
+
+func _process(delta):
+	if (!is_dragging) and (current_spot):
+		position = current_spot.global_position
 
 func _physics_process(delta):
 	if is_dragging == true:
@@ -36,7 +42,9 @@ func _input(event):
 					and
 					(drop_spot.get_overlapping_areas().size() == 1)
 				):
-					var snap_position = drop_spot.position
+					snap_position = drop_spot.global_position
+					current_spot = drop_spot
+					self.reparent(drop_spot)
 					var tween = get_tree().create_tween()
 					tween.tween_property(
 						self,
@@ -45,4 +53,6 @@ func _input(event):
 						delay
 					)
 					return
+			self.reparent(get_tree().get_nodes_in_group("Bag")[0])
+			current_spot = null
 			self.position = start_position
