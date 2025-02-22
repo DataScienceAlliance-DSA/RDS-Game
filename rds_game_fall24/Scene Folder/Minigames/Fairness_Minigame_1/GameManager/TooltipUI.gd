2df8bae74de172aadc90eee59ee985713845636d
Diego Pereyra <23diegopereyra@gmail.com>
2025-02-21 16:54:15 -0800

    ui imports, tilemap imports (fairness game #1)

---
+++ rds_game_fall24/Scene Folder/Minigames/Fairness_Minigame_1/GameManager/TooltipUI.gd
@@ -0,0 +1,15 @@
+extends TextureRect
+
+@onready var active_png = load("res://assets/Fairness_Village/Help Button_Active.png")
+@onready var hover_png = load("res://assets/Fairness_Village/Help Button_Hover.png")
+@onready var click_png = load("res://assets/Fairness_Village/Help Button_On-Click.png")
+@onready var visit_png = load("res://assets/Fairness_Village/Help Button_Visited.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
