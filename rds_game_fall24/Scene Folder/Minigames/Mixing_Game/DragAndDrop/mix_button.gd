a252ed2cd43240fc25e8e94d1e003c42197aa7a7
Kyuowo <otang@ucsd.edu>
2025-04-10 15:49:30 -0700

    new buttons

--- rds_game_fall24/Scene Folder/Minigames/Mixing_Game/DragAndDrop/mix_button.gd
+++ rds_game_fall24/Scene Folder/Minigames/Mixing_Game/DragAndDrop/mix_button.gd
@@ -1,16 +1,8 @@
-extends TextureRect
+extends TextureButton
 
 @onready var button_primed : bool = false	# mouse over button
 @onready var canvas : CanvasItem = self as CanvasItem
 
 func _process(delta):
-	if button_primed and Input.is_action_just_pressed("left_click"):
+	if Input.is_action_just_pressed("left_click"):
 		get_tree().call_group("MixingGame", "mix")
-
-func _on_mouse_entered():
-	canvas.modulate = Color(215.0/255.0, 155.0/255.0, 25.0/255.0, 1.0)
-	button_primed = true
-
-func _on_mouse_exited():
-	canvas.modulate = Color(254.0/255.0, 206.0/255.0, 126.0/255.0, 1.0)
-	button_primed = false
