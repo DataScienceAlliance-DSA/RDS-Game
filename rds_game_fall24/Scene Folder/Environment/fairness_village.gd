b774caea0763a890cd085974a764c81a31b47c90
Diego Pereyra <23diegopereyra@gmail.com>
2025-04-11 17:08:43 -0700

    forgor to push

--- rds_game_fall24/Scene Folder/Environment/fairness_village.gd
+++ rds_game_fall24/Scene Folder/Environment/fairness_village.gd
@@ -3,10 +3,12 @@ extends Node2D
 @onready var player = get_tree().get_nodes_in_group("Player")[0]
 
 @export var default_spawn : Vector2
+@onready var crystal = $PodiumGroup/Crystal
+
+var cm : CutsceneManager # cutscene manager for this 
 
 func _ready():
 	player.global_position = PS.spawning_at
-	player.autonomous = false
 	UI.start_scene_change(false, false, "")
 	
 	match(PS.village_state):
@@ -17,7 +19,24 @@ func _ready():
 			if PS.spawning_at != Vector2(0.,0.): player.global_posiion = PS.spawning_at
 			else: player.global_position = default_spawn
 		3:
+			player.autonomous = true
+			print(player.autonomous)
+			
 			player.global_position = Vector2(1629,-1839)
-
-func _process(delta):
-	print(player.global_position)
+			player.reset_camera()
+			
+			var crystal_rises = UniqueAction.new(crystal, Callable(crystal, "levitate"))
+			var actions : Array[Action] = [crystal_rises]
+			for action in actions:
+				add_child(action)
+			
+			cm = CutsceneManager.new(actions)
+			add_child(cm)
+			
+			cm.series_action()
+			
+			await cm.actions_complete
+			
+			cm.call_dialogue("res://Scripts/Dialogues/Fairness/FairnessCrystal2.json")
+			
+			await cm.lines_complete
