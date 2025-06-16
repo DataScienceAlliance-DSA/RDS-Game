09c73ca7412527339f871577b4f3b507a841aa31
Diego Pereyra <23diegopereyra@gmail.com>
2025-06-16 14:34:48 -0700

    fixed credits fade

--- rds_game_fall24/Scenes/end_credits.gd
+++ rds_game_fall24/Scenes/end_credits.gd
@@ -1,14 +1,31 @@
 extends Node2D
 
 @onready var text = $CanvasLayer/MarginContainer
+@onready var fade_color = $CanvasLayer2/ColorRect
+
+@onready var fading_screen = false
+@onready var fade_timer = 0.
 
 func _ready():
 	UI.start_scene_change(false, false, "")
 	UI.set_tooltip("")
 
 func _process(delta):
-	text.position.y -= delta * 30.
-	
-	if (text.position.y < -1874.): 
-		UI.start_scene_change(true, true, "res://Scenes/scene_selection.tscn")
-		set_process(false)
+	if (!fading_screen):
+		text.position.y -= delta * 30.
+		
+		if (text.position.y < -2854.):
+			set_process(false)
+			finish_credits()
+	else:
+		fade_timer = fade_timer + delta
+		if (fade_timer < 3.):
+			fade_color.color.a = fade_timer / 3.
+		else:
+			UI.start_scene_change(true, true, "res://Scenes/scene_selection.tscn")
+			set_process(false)
+
+func finish_credits():
+	await get_tree().create_timer(5.0).timeout
+	fading_screen = true
+	set_process(true)
