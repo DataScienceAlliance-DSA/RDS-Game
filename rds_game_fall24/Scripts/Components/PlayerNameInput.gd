88621137e9860beca41ecbc47a281d3850717994
Diego Pereyra <23diegopereyra@gmail.com>
2025-06-18 10:59:54 -0700

    revert stuff, player naming ui setup, fix functionality

--- rds_game_fall24/Scripts/Components/PlayerNameInput.gd
+++ rds_game_fall24/Scripts/Components/PlayerNameInput.gd
@@ -2,12 +2,12 @@ extends Control
 
 signal confirmed
 
-@onready var name_input = $PlayerName
-@onready var confirm_button = $VBoxContainer/ConfirmButton
+@onready var name_input = $MarginContainer/PlayerNamePanel/MarginContainer/VBoxContainer/PlayerName
+@onready var confirm_button = $MarginContainer/PlayerNamePanel/MarginContainer/VBoxContainer/ConfirmButton
 
 func _ready():
 	confirm_button.pressed.connect(_on_confirm_button_pressed)
-	
+
 func _on_confirm_button_pressed():
 	var name = name_input.text.strip_edges()
 	if name != "":
