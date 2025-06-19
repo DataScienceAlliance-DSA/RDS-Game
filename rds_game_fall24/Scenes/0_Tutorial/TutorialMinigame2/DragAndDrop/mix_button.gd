13605bb5e739899f2d0d49dc4aa1f28837e68ed4
Diego Pereyra <23diegopereyra@gmail.com>
2025-06-18 21:40:34 -0700

    mixing game fixes redo

--- rds_game_fall24/Scenes/0_Tutorial/TutorialMinigame2/DragAndDrop/mix_button.gd
+++ rds_game_fall24/Scenes/0_Tutorial/TutorialMinigame2/DragAndDrop/mix_button.gd
@@ -3,6 +3,5 @@ extends TextureButton
 @onready var button_primed : bool = false	# mouse over button
 @onready var canvas : CanvasItem = self as CanvasItem
 
-func _process(delta):
-	if Input.is_action_just_pressed("left_click"):
-		get_tree().call_group("MixingGame", "mix")
+func _on_pressed():
+	get_tree().call_group("MixingGame", "mix")
