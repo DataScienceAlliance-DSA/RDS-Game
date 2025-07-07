d39a8be8fdf1325279ff8319881ec034857e3303
ColorblindEntity <firestormthedm@gmail.com>
2025-07-07 09:13:42 -0700

    Bug Fixes

--- rds_game_fall24/Scripts/Characters/Fox/fox.gd
+++ rds_game_fall24/Scripts/Characters/Fox/fox.gd
@@ -1,15 +1,17 @@
 extends CharacterController
 
 @onready var player = get_tree().get_nodes_in_group("Player")[0]
+@onready var following_player = false
 
 func _ready():
 	super()
 
 func _process(delta):
-	if (!autonomous):
-		target_pos = player.global_position
-		autonomous = true
-		print("OFF")
-	else:
-		print("AUTONOMOUS")
-		super(delta)
+	if following_player:
+		if (!autonomous):
+			target_pos = player.global_position
+			autonomous = true
+			print("OFF")
+		else:
+			print("AUTONOMOUS")
+			super(delta)
