e9ae8a32f84432ae7138ccba113a1a79b9b7d11a
ColorblindEntity <firestormthedm@gmail.com>
2025-07-10 11:26:32 -0700

    Privacy Minigame 2
    This is mostly setting up variables and implementing the scaling numbers.  With the current timer, once it begins, the game will take exactly 3 minutes.

---
+++ rds_game_fall24/Scripts/3_Privacy/PrivacyMinigame2/Tower.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+@onready var rangeshape = $CollisionShape2D
+var bullet_path = preload("res://Scenes/3_Privacy/PrivacyMinigame2/Fireball.tscn")
+var range : int = 50 #sets range
+var damage : int = 3 #sets damage
+var atksp : int = 2 #sets attack speed
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	pass
--- rds_game_fall24/Scripts/3_Privacy/PrivacyMinigame2/Tower_Defence_Game.gd
+++ rds_game_fall24/Scripts/3_Privacy/PrivacyMinigame2/Tower_Defence_Game.gd
@@ -1,7 +1,7 @@
 extends Node
 var scaling : int = 0 #Scaling for enemy health/speed
 var ap :int = 0 #These are your Action Points, the currency in the game 
-var done : int = 0#This is how close to done the node is in terms of uploading the information
+@onready var done = 1#This is how close to done the node is in terms of uploading the information
 var life : int = 15 #This is how many lives you have
 
 # Called when the node enters the scene tree for the first time.
@@ -11,7 +11,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	#This set of if statements sets the difficulty of the game
+	if(scaling == 0):
+		if(done >= 25):
+			scaling += 1
+			
+	if(scaling == 1):
+		if(done >= 50):
+			scaling += 1
+	if(scaling == 2):
+		if(done >= 75):
+			scaling +=1
 
 
 func _on_timer_timeout():
